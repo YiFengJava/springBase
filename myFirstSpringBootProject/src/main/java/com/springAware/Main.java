d6118fb76f76f10a87a1da20c4639a68b93494cd
YiFengJava <2260213493@qq.com>
2018-11-15 19:13:23 +0800

    springsecurity学习练习项目

--- myFirstSpringBootProject/src/main/java/com/springAware/Main.java
+++ myFirstSpringBootProject/src/main/java/com/springAware/Main.java
@@ -1,10 +1,13 @@
 package com.springAware;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class Main{
-
+//    DispatcherServlet
     public static void main(String[] args) {
+//        BeanFactory
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AwareConfig.class);
         AwareService bean = context.getBean(AwareService.class);
         bean.outputResult();
