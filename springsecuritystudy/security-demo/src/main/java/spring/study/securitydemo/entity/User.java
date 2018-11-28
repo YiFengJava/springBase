2f5f6c6be27f9905334fe2f0c8800db6bb293d77
YiFengJava <2260213493@qq.com>
2018-11-28 15:20:06 +0800

    微信 session

--- springsecuritystudy/security-demo/src/main/java/spring/study/securitydemo/entity/User.java
+++ springsecuritystudy/security-demo/src/main/java/spring/study/securitydemo/entity/User.java
@@ -1,63 +1,240 @@
 package spring.study.securitydemo.entity;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.social.security.SocialUserDetails;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.*;
 
 @Entity
 @Table(name="tb_user")
-public class User  implements UserDetails ,SocialUserDetails {
+public class User  implements UserDetails ,SocialUserDetails,Serializable {
     @Id
-    private  String userId ="10";
+    private  String userId;    //主键
 
-    @Column
-    private String username="yudong";
+    @Column(name="username",unique=true,nullable=false)
+    private String username;  //用户名
 
-    @Column
-    private  String password="123456";
+    @Column(name="nick_name",nullable=false)
+    private String nickName; //昵称
 
-    @Transient
-    private Set<Permissions> authorities=new HashSet<>();
+    @Column(name="password",unique=false,nullable=false)
+    private  String password;  //密码
+
+    @Column(name = "mobile",length = 20)
+    private String mobile;  //电话
+
+    @Column(name = "handImage")
+    private String handImge;  //图像
+
+    @Column(name = "age",length = 3)
+    private Integer age;  //年龄
+
+    @Column(name = "sex",length = 1)
+    private Integer sex; //性别
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birthday",nullable=false)
+    //出参时间格式化
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    //入参时，请求报文只需要传入yyyymmddhhmmss字符串进来，则自动转换为Date类型数据
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthday;    //出生日期
+
+    @Column(name = "dateStatus",length = 1)
+    private Integer dateStatus=0;   //数据状态
+
+    @Temporal(TemporalType.DATE)
+    @Column(name ="created_time" )
+    //出参时间格式化
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    //入参时，请求报文只需要传入yyyymmddhhmmss字符串进来，则自动转换为Date类型数据
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createdTime;   //创建时间
+
+    @Column(name = "created_emp")
+    private String createdEmp;   //创建人
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_time")
+    //出参时间格式化
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    //入参时，请求报文只需要传入yyyymmddhhmmss字符串进来，则自动转换为Date类型数据
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updatedTime;  //更新时间
+
+    @Column(name = "updated_emp")
+    private String updatedEmp; //更新人
+
+    @Column(name = "version")
+    private String version;  //乐观锁字段
+
+    @Column(name = "reserve1")
+    private String reserve1;  //备用字段
+
+    @Column(name = "reserve2")
+    private String reserve2;  //备用字段
+
+    @Column(name = "reserve3")
+    private String reserve3;  //备用字段
 
     @ManyToMany(fetch=FetchType.EAGER)
     @JoinTable(name = "tb_role_user",joinColumns = { @JoinColumn(referencedColumnName = "userId",name="uid") }, inverseJoinColumns = {
             @JoinColumn(referencedColumnName = "id",name="rid") })
     private Set<Role> roleSet=new HashSet<>();
 
+    @Transient
+    private Set<Permissions> authorities=new HashSet<>();
+
     @Override
     public String getUserId() {
-        return this.userId;
+        return userId;
     }
 
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-
-    public void setAuthorities(Set<Permissions> authorities) {
-        this.authorities = authorities;
+    public String getMobile() {
+        return mobile;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.authorities;
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
     }
 
-    @Override
-    public String getPassword() {
+    public String getHandImge() {
+        return handImge;
+    }
 
-        return this.password;
+    public void setHandImge(String handImge) {
+        this.handImge = handImge;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getDateStatus() {
+        return dateStatus;
+    }
+
+    public void setDateStatus(Integer dateStatus) {
+        this.dateStatus = dateStatus;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getCreatedEmp() {
+        return createdEmp;
+    }
+
+    public void setCreatedEmp(String createdEmp) {
+        this.createdEmp = createdEmp;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    public String getUpdatedEmp() {
+        return updatedEmp;
+    }
+
+    public void setUpdatedEmp(String updatedEmp) {
+        this.updatedEmp = updatedEmp;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getReserve1() {
+        return reserve1;
+    }
+
+    public void setReserve1(String reserve1) {
+        this.reserve1 = reserve1;
+    }
+
+    public String getReserve2() {
+        return reserve2;
+    }
+
+    public void setReserve2(String reserve2) {
+        this.reserve2 = reserve2;
+    }
+
+    public String getReserve3() {
+        return reserve3;
+    }
+
+    public void setReserve3(String reserve3) {
+        this.reserve3 = reserve3;
     }
 
     public Set<Role> getRoleSet() {
@@ -69,57 +246,32 @@ public class User  implements UserDetails ,SocialUserDetails {
     }
 
     @Override
-    public String getUsername() {
-        return this.username;
+    public Set<Permissions> getAuthorities() {
+        return authorities;
     }
+
+    public void setAuthorities(Set<Permissions> authorities) {
+        this.authorities = authorities;
+    }
+
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return false;
     }
-
-    // 封装了权限信息
-//    Collection<? extends GrantedAuthority> getAuthorities(){
-//        return  null ;
-//    };
-//    // 密码信息
-//    String getPassword(){
-//        return  null;
-//    };
-//    // 登录用户名
-//    String getUsername(){
-//        return  null;
-//    };
-//    // 帐户是否过期
-//    boolean isAccountNonExpired(){
-//        return  true;
-//    };
-//    // 帐户是否被冻结
-//    boolean isAccountNonLocked(){
-//        return  true;
-//    };
-//    // 帐户密码是否过期，一般有的密码要求性高的系统会使用到，比较每隔一段时间就要求用户重置密码
-//    boolean isCredentialsNonExpired(){
-//        return  true;
-//    };
-//    // 帐号是否可用
-//    boolean isEnabled(){
-//        return  true;
-//    };//////////////
-
 }
