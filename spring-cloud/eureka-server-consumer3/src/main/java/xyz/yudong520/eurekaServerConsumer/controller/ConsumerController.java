9a856a9bd82aaca73fb3b906754aa0668cc93528
YiFengJava <2260213493@qq.com>
2019-01-02 11:28:47 +0800

    随时

--- spring-cloud/eureka-server-consumer3/src/main/java/xyz/yudong520/eurekaServerConsumer/controller/ConsumerController.java
+++ spring-cloud/eureka-server-consumer3/src/main/java/xyz/yudong520/eurekaServerConsumer/controller/ConsumerController.java
@@ -1,6 +1,8 @@
 package xyz.yudong520.eurekaServerConsumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,17 @@ public class ConsumerController {
     @Autowired
     private HelloRemote helloRemote;
 
+    @Value("${yd}")
+    private String yd;
+
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
         return helloRemote.hello(name);
+    }
+
+    @GetMapping("/abc")
+    public String jij(){
+        return yd;
     }
 
 }
